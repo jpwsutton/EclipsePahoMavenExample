850f7ec40db018a507e3106a9e319c231703eeee
James Sutton <james.sutton@uk.ibm.com>
2015-10-06 10:14:03 +0100

    Changing server to iot.eclipse.org

--- src/main/java/org/eclipse/paho/App.java
+++ src/main/java/org/eclipse/paho/App.java
@@ -17,7 +17,7 @@ public class App
       String topic        = "MQTT Examples";
         String content      = "Message from MqttPublishSample";
         int qos             = 2;
-        String broker       = "ws://localhost:1883";
+        String broker       = "ws://iot.eclipse.org:80";
         String clientId     = "JavaSample";
         MemoryPersistence persistence = new MemoryPersistence();
 
